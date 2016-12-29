a0392696a951b4013d7e6f041b7e37ad8363b2bb
Renaud Trezieres <renaud.trezieres@gmail.com>
2016-12-29 19:01:52 +0100

    interface test

--- src/main/java/com/project/thurnandtaxis/controler/InterfaceTest.java
+++ src/main/java/com/project/thurnandtaxis/controler/InterfaceTest.java
@@ -2,6 +2,8 @@ package com.project.thurnandtaxis.controler;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
 import javax.swing.ImageIcon;
@@ -9,115 +11,264 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class InterfaceTest extends JFrame {
-    private final JPanel panel_1 = new JPanel();
+    private final JPanel panelCardsHandAndRoad = new JPanel();
     
     public InterfaceTest() {
         this.setTitle("Thurn and Taxis");
         this.setResizable(false);
-        this.getContentPane().add(this.panel_1, BorderLayout.SOUTH);
-        this.panel_1.setLayout(new GridLayout(1, 2, 0, 0));
+        this.getContentPane().add(this.panelCardsHandAndRoad, BorderLayout.SOUTH);
+        this.panelCardsHandAndRoad.setLayout(new GridLayout(1, 2, 0, 0));
 
-        JPanel panel_4 = new JPanel();
-        this.panel_1.add(panel_4);
-        panel_4.setLayout(new GridLayout(1, 6, 0, 0));
+        JPanel panelCardHand = new JPanel();
+        this.panelCardsHandAndRoad.add(panelCardHand);
+        panelCardHand.setLayout(new GridLayout(1, 6, 0, 0));
+
+        Dimension dimCard = new Dimension(10, 150);
 
         JButton btnNewButton = new JButton("Card1");
+        btnNewButton.setPreferredSize(dimCard);
         btnNewButton.setBackground(new Color(205, 133, 63));
-        btnNewButton.setForeground(new Color(205, 133, 63));
-        panel_4.add(btnNewButton);
+        btnNewButton.setForeground(new Color(255, 215, 0));
+        panelCardHand.add(btnNewButton);
 
         JButton btnNewButton_1 = new JButton("Card2");
-        panel_4.add(btnNewButton_1);
+        btnNewButton_1.setPreferredSize(dimCard);
+        panelCardHand.add(btnNewButton_1);
 
         JButton btnNewButton_2 = new JButton("Card3");
-        panel_4.add(btnNewButton_2);
+        btnNewButton_2.setPreferredSize(dimCard);
+        panelCardHand.add(btnNewButton_2);
 
         JButton btnNewButton_3 = new JButton("Card4");
-        panel_4.add(btnNewButton_3);
+        btnNewButton_3.setPreferredSize(dimCard);
+        panelCardHand.add(btnNewButton_3);
 
         JButton btnNewButton_4 = new JButton("Card5");
-        panel_4.add(btnNewButton_4);
+        btnNewButton_4.setPreferredSize(dimCard);
+        panelCardHand.add(btnNewButton_4);
 
         JButton btnNewButton_5 = new JButton("Card6");
-        panel_4.add(btnNewButton_5);
+        btnNewButton_5.setPreferredSize(dimCard);
+        panelCardHand.add(btnNewButton_5);
 
-        JPanel panel_5 = new JPanel();
-        this.panel_1.add(panel_5);
-        panel_5.setLayout(new GridLayout(1, 6, 0, 0));
+        JPanel panelCardRoad = new JPanel();
+        this.panelCardsHandAndRoad.add(panelCardRoad);
+        panelCardRoad.setLayout(new GridLayout(1, 6, 0, 0));
 
         JButton btnRoad = new JButton("Road1");
-        panel_5.add(btnRoad);
+        btnRoad.setPreferredSize(dimCard);
+        panelCardRoad.add(btnRoad);
 
         JButton btnRoad_1 = new JButton("Road2");
-        panel_5.add(btnRoad_1);
+        btnRoad_1.setPreferredSize(dimCard);
+        panelCardRoad.add(btnRoad_1);
 
         JButton btnRoad_2 = new JButton("Road3");
-        panel_5.add(btnRoad_2);
+        btnRoad_2.setPreferredSize(dimCard);
+        panelCardRoad.add(btnRoad_2);
 
         JButton btnRoad_3 = new JButton("Road4");
-        panel_5.add(btnRoad_3);
+        btnRoad_3.setPreferredSize(dimCard);
+        panelCardRoad.add(btnRoad_3);
 
         JButton btnRoad_4 = new JButton("Road5");
-        panel_5.add(btnRoad_4);
+        btnRoad_4.setPreferredSize(dimCard);
+        panelCardRoad.add(btnRoad_4);
 
         JButton btnRoad_5 = new JButton("Road6");
-        panel_5.add(btnRoad_5);
+        btnRoad_5.setPreferredSize(dimCard);
+        panelCardRoad.add(btnRoad_5);
 
-        JPanel panel = new JPanel();
-        this.getContentPane().add(panel, BorderLayout.CENTER);
+        JPanel panelThurnPlan = new JPanel();
+        FlowLayout fl_panelThurnPlan = (FlowLayout) panelThurnPlan.getLayout();
+        fl_panelThurnPlan.setAlignment(FlowLayout.LEADING);
+        fl_panelThurnPlan.setVgap(0);
+        fl_panelThurnPlan.setHgap(0);
+        this.getContentPane().add(panelThurnPlan, BorderLayout.CENTER);
         
-        JLabel lblNewLabel = new JLabel("");
-        lblNewLabel.setIcon(new ImageIcon(InterfaceTest.class.getResource("/com/project/thurnandtaxis/statics/img/thurnplan.jpg")));
-        panel.add(lblNewLabel);
+        JLabel lblThurnplan = new JLabel("");
+        lblThurnplan.setHorizontalAlignment(SwingConstants.CENTER);
+        lblThurnplan.setIcon(new ImageIcon(InterfaceTest.class.getResource("/com/project/thurnandtaxis/statics/img/thurnplan.jpg")));
+        panelThurnPlan.add(lblThurnplan);
         
-        JPanel panel_2 = new JPanel();
-        this.getContentPane().add(panel_2, BorderLayout.EAST);
-        panel_2.setLayout(new GridLayout(4, 2, 2, 2));
+        JPanel panelPlayers = new JPanel();
+        this.getContentPane().add(panelPlayers, BorderLayout.EAST);
+        panelPlayers.setLayout(new GridLayout(4, 2, 2, 2));
         
-        JLabel lblNewLabel_1 = new JLabel("Player 1");
-        panel_2.add(lblNewLabel_1);
+        JLabel lblPlayer1 = new JLabel("Player 1");
+        panelPlayers.add(lblPlayer1);
         
-        JPanel panel_3 = new JPanel();
-        panel_2.add(panel_3);
-        panel_3.setLayout(new GridLayout(6, 2, 2, 2));
+        JLabel lblPlayer2 = new JLabel("Player 2");
+        panelPlayers.add(lblPlayer2);
+        
+        JPanel panelPlayer1 = new JPanel();
+        panelPlayers.add(panelPlayer1);
+        panelPlayer1.setLayout(new GridLayout(6, 2, 2, 2));
         
         JLabel lblNewLabel_2 = new JLabel("New label");
-        panel_3.add(lblNewLabel_2);
+        panelPlayer1.add(lblNewLabel_2);
         
         JLabel lblNewLabel_3 = new JLabel("New label");
-        panel_3.add(lblNewLabel_3);
+        panelPlayer1.add(lblNewLabel_3);
         
         JLabel lblNewLabel_4 = new JLabel("New label");
-        panel_3.add(lblNewLabel_4);
+        panelPlayer1.add(lblNewLabel_4);
         
         JLabel lblNewLabel_5 = new JLabel("New label");
-        panel_3.add(lblNewLabel_5);
+        panelPlayer1.add(lblNewLabel_5);
         
         JLabel lblNewLabel_6 = new JLabel("New label");
-        panel_3.add(lblNewLabel_6);
+        panelPlayer1.add(lblNewLabel_6);
         
         JLabel lblNewLabel_7 = new JLabel("New label");
-        panel_3.add(lblNewLabel_7);
+        panelPlayer1.add(lblNewLabel_7);
         
         JLabel lblNewLabel_8 = new JLabel("New label");
-        panel_3.add(lblNewLabel_8);
+        panelPlayer1.add(lblNewLabel_8);
         
         JLabel lblNewLabel_9 = new JLabel("New label");
-        panel_3.add(lblNewLabel_9);
+        panelPlayer1.add(lblNewLabel_9);
         
         JLabel lblNewLabel_10 = new JLabel("New label");
-        panel_3.add(lblNewLabel_10);
+        panelPlayer1.add(lblNewLabel_10);
         
         JLabel lblNewLabel_11 = new JLabel("New label");
-        panel_3.add(lblNewLabel_11);
+        panelPlayer1.add(lblNewLabel_11);
         
         JLabel lblNewLabel_12 = new JLabel("New label");
-        panel_3.add(lblNewLabel_12);
+        panelPlayer1.add(lblNewLabel_12);
         
         JLabel lblNewLabel_13 = new JLabel("New label");
-        panel_3.add(lblNewLabel_13);
+        panelPlayer1.add(lblNewLabel_13);
+        
+        JPanel panelPlayer2 = new JPanel();
+        panelPlayers.add(panelPlayer2);
+        panelPlayer2.setLayout(new GridLayout(6, 2, 2, 2));
+        
+        JLabel label = new JLabel("New label");
+        panelPlayer2.add(label);
+        
+        JLabel label_1 = new JLabel("New label");
+        panelPlayer2.add(label_1);
+        
+        JLabel label_2 = new JLabel("New label");
+        panelPlayer2.add(label_2);
+        
+        JLabel label_3 = new JLabel("New label");
+        panelPlayer2.add(label_3);
+        
+        JLabel label_4 = new JLabel("New label");
+        panelPlayer2.add(label_4);
+        
+        JLabel label_5 = new JLabel("New label");
+        panelPlayer2.add(label_5);
+        
+        JLabel label_6 = new JLabel("New label");
+        panelPlayer2.add(label_6);
+        
+        JLabel label_7 = new JLabel("New label");
+        panelPlayer2.add(label_7);
+        
+        JLabel label_8 = new JLabel("New label");
+        panelPlayer2.add(label_8);
+        
+        JLabel label_9 = new JLabel("New label");
+        panelPlayer2.add(label_9);
+        
+        JLabel label_10 = new JLabel("New label");
+        panelPlayer2.add(label_10);
+        
+        JLabel label_11 = new JLabel("New label");
+        panelPlayer2.add(label_11);
+        
+        JLabel lblPlayer3 = new JLabel("Player 3");
+        panelPlayers.add(lblPlayer3);
+        
+        JLabel lblPlayer4 = new JLabel("Player 4");
+        panelPlayers.add(lblPlayer4);
+        
+        JPanel panelPlayer3 = new JPanel();
+        panelPlayers.add(panelPlayer3);
+        panelPlayer3.setLayout(new GridLayout(6, 2, 2, 2));
+        
+        JLabel label_12 = new JLabel("New label");
+        panelPlayer3.add(label_12);
+        
+        JLabel label_13 = new JLabel("New label");
+        panelPlayer3.add(label_13);
+        
+        JLabel label_14 = new JLabel("New label");
+        panelPlayer3.add(label_14);
+        
+        JLabel label_15 = new JLabel("New label");
+        panelPlayer3.add(label_15);
+        
+        JLabel label_16 = new JLabel("New label");
+        panelPlayer3.add(label_16);
+        
+        JLabel label_17 = new JLabel("New label");
+        panelPlayer3.add(label_17);
+        
+        JLabel label_18 = new JLabel("New label");
+        panelPlayer3.add(label_18);
+        
+        JLabel label_19 = new JLabel("New label");
+        panelPlayer3.add(label_19);
+        
+        JLabel label_20 = new JLabel("New label");
+        panelPlayer3.add(label_20);
+        
+        JLabel label_21 = new JLabel("New label");
+        panelPlayer3.add(label_21);
+        
+        JLabel label_22 = new JLabel("New label");
+        panelPlayer3.add(label_22);
+        
+        JLabel label_23 = new JLabel("New label");
+        panelPlayer3.add(label_23);
+        
+        JPanel panelPlayer4 = new JPanel();
+        panelPlayers.add(panelPlayer4);
+        panelPlayer4.setLayout(new GridLayout(6, 2, 2, 2));
+        
+        JLabel label_24 = new JLabel("New label");
+        panelPlayer4.add(label_24);
+        
+        JLabel label_25 = new JLabel("New label");
+        panelPlayer4.add(label_25);
+        
+        JLabel label_26 = new JLabel("New label");
+        panelPlayer4.add(label_26);
+        
+        JLabel label_27 = new JLabel("New label");
+        panelPlayer4.add(label_27);
+        
+        JLabel label_28 = new JLabel("New label");
+        panelPlayer4.add(label_28);
+        
+        JLabel label_29 = new JLabel("New label");
+        panelPlayer4.add(label_29);
+        
+        JLabel label_30 = new JLabel("New label");
+        panelPlayer4.add(label_30);
+        
+        JLabel label_31 = new JLabel("New label");
+        panelPlayer4.add(label_31);
+        
+        JLabel label_32 = new JLabel("New label");
+        panelPlayer4.add(label_32);
+        
+        JLabel label_33 = new JLabel("New label");
+        panelPlayer4.add(label_33);
+        
+        JLabel label_34 = new JLabel("New label");
+        panelPlayer4.add(label_34);
+        
+        JLabel label_35 = new JLabel("New label");
+        panelPlayer4.add(label_35);
         
         this.setSize(1600, 900);
         this.setVisible(true);
