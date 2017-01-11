15eb4f28089e1353b62b014019c1afd4c151fa3d
Renaud Trezieres <renaud.trezieres@gmail.com>
2017-01-11 17:48:41 +0100

    refacto serviceActionButton (à revoir)

--- src/main/java/com/project/thurnandtaxis/services/ServiceActionButton.java
+++ src/main/java/com/project/thurnandtaxis/services/ServiceActionButton.java
@@ -17,48 +17,64 @@ import com.project.thurnandtaxis.data.beans.Player;
 import com.project.thurnandtaxis.data.enumerations.EnumOfficials;
 
 public class ServiceActionButton {
-
+    
     private ServiceCards serviceCards;
     private JButton btnDeckCard;
     private JLabel lblCardRemaining;
     private List<JButton> listCardsHand;
     private List<JButton> listCardsRoad;
-
-    public ServiceActionButton(final JButton btnDeckCard, JLabel lblCardRemaining, List<JButton> listCardsHand, List<JButton> listCardsRoad) {
+    private List<JButton> listeButtonsOfficials;
+    private List<JButton> listCardsVisible;
+    
+    public ServiceActionButton(final JButton btnDeckCard, JLabel lblCardRemaining, List<JButton> listCardsHand, List<JButton> listCardsRoad,
+        List<JButton> listeButtonsOfficials, List<JButton> listCardsVisible) {
         this.serviceCards = new ServiceCards();
         this.btnDeckCard = btnDeckCard;
         this.lblCardRemaining = lblCardRemaining;
         this.listCardsHand = listCardsHand;
         this.listCardsRoad = listCardsRoad;
+        this.listeButtonsOfficials = listeButtonsOfficials;
+        this.listCardsVisible = listCardsVisible;
     }
-
+    
     public void addActionButtonDeckCard(final Player player, final List<CityCard> listCardRemaining) {
         this.btnDeckCard.addActionListener(new ActionListener() {
-            
+
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (CollectionUtils.isNotEmpty(listCardRemaining)) {
-                    ServiceActionButton.this.serviceCards.takeOneCityCard(player, listCardRemaining, ServiceActionButton.this.listCardsHand);
-                    ServiceActionButton.this.updateLabelCardRemaining(listCardRemaining);
+                    if (ServiceActionButton.this.isListeCardsHandFull(ServiceActionButton.this.listCardsHand)) {
+                        ServiceActionButton.this.serviceCards.takeOneCityCard(player, listCardRemaining, ServiceActionButton.this.listCardsHand);
+                        ServiceActionButton.this.updateLabelCardRemaining(listCardRemaining);
+                    } else {
+                        JOptionPane.showMessageDialog(null, "You can't take another card.");
+                    }
                 } else {
-                    JOptionPane.showMessageDialog(null, "Do you want shuffle cards ?");
+                    JOptionPane.showMessageDialog(null, "Do you want to shuffle cards ?");
                 }
             }
+            
         });
     }
-    
+
     protected void updateLabelCardRemaining(List<CityCard> listCardRemaining) {
         this.lblCardRemaining.setText(String.valueOf(listCardRemaining.size()));
     }
     
-    public void addActionButtonAdministrator(final JButton btnAdministrator, final List<CityCard> listeCityCards,
-        final List<JButton> listeCardsVisible) {
+    public void addActionsButtonsOfficials(List<CityCard> listeCityCards) {
+        this.addActionButtonAdministrator(this.getOfficialByName(EnumOfficials.ADMINISTRATOR.getName()), listeCityCards);
+        this.addActionButtonCartwright(this.getOfficialByName(EnumOfficials.CARTWRIGHT.getName()), listeCityCards);
+        this.addActionButtonPostalCarrier(this.getOfficialByName(EnumOfficials.POSTAL_CARRIER.getName()), listeCityCards);
+        this.addActionButtonPostmaster(this.getOfficialByName(EnumOfficials.POSTMASTER.getName()), listeCityCards);
+    }
+    
+    private void addActionButtonAdministrator(final JButton btnAdministrator, final List<CityCard> listeCityCards) {
         btnAdministrator.addActionListener(new ActionListener() {
-            
+
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (CollectionUtils.isNotEmpty(listeCityCards)) {
-                    for (JButton cardVisible : listeCardsVisible) {
+                    for (JButton cardVisible : ServiceActionButton.this.listCardsVisible) {
                         ServiceActionButton.this.serviceCards.addCardVisible(listeCityCards, cardVisible);
                         listeCityCards.remove(Iterables.getLast(listeCityCards));
                         ServiceActionButton.this.updateLabelCardRemaining(listeCityCards);
@@ -66,42 +82,42 @@ public class ServiceActionButton {
                 } else {
                     JOptionPane.showInternalMessageDialog(null, "Plus assez de cartes.");
                 }
-                System.out.println("cards remaining : " + listeCityCards.size());
             }
         });
-        
+
+    }
+    
+    private void addActionButtonCartwright(final JButton btnCartwright, List<CityCard> listeCityCards) {
+        // TODO Auto-generated method stub
+
     }
 
-    public void addActionButtonCartwright(final JButton btnCartwright, List<CityCard> listeCityCards) {
+    private void addActionButtonPostalCarrier(final JButton btnPostalCarrier, List<CityCard> listeCityCards) {
         // TODO Auto-generated method stub
         
     }
-    
-    public void addActionButtonPostalCarrier(final JButton btnPostalCarrier, List<CityCard> listeCityCards) {
+
+    private void addActionButtonPostmaster(final JButton btnPostmaster, List<CityCard> listeCityCards) {
         // TODO Auto-generated method stub
-
+        
     }
-    
-    public void addActionButtonPostmaster(final JButton btnPostmaster, List<CityCard> listeCityCards) {
-        // TODO Auto-generated method stub
 
-    }
-    
-    public void addActionsButtonsOfficials(List<JButton> listeButtonsOfficials, List<CityCard> listeCityCards, List<JButton> listeCardsVisible) {
-        this.addActionButtonAdministrator(getOfficial(listeButtonsOfficials, EnumOfficials.ADMINISTRATOR.getName()), listeCityCards,
-                        listeCardsVisible);
-        this.addActionButtonCartwright(getOfficial(listeButtonsOfficials, EnumOfficials.CARTWRIGHT.getName()), listeCityCards);
-        this.addActionButtonPostalCarrier(getOfficial(listeButtonsOfficials, EnumOfficials.POSTAL_CARRIER.getName()), listeCityCards);
-        this.addActionButtonPostmaster(getOfficial(listeButtonsOfficials, EnumOfficials.POSTMASTER.getName()), listeCityCards);
-    }
-    
-    private static JButton getOfficial(List<JButton> listeButtonsOfficials, String nameOfficial) {
-
-        for (JButton btn : listeButtonsOfficials) {
+    private JButton getOfficialByName(String nameOfficial) {
+        
+        for (JButton btn : this.listeButtonsOfficials) {
             if (StringUtils.equals(btn.getToolTipText(), nameOfficial)) {
                 return btn;
             }
         }
         return null;
+    }
+    
+    private boolean isListeCardsHandFull(List<JButton> listCardsHand) {
+        for (JButton btn : listCardsHand) {
+            if (StringUtils.isBlank(btn.getToolTipText())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
