a7b0108031509fcfeaaba3ef7b299f7a743854b4
Renaud Trezieres <renaud.trezieres@capgemini.com>
2017-02-07 18:12:20 +0100

    add label info

--- src/main/java/data/beans/principal/AllLabels.java
+++ src/main/java/data/beans/principal/AllLabels.java
@@ -3,15 +3,24 @@ package data.beans.principal;
 import javax.swing.JLabel;
 
 public class AllLabels {
-    
+
     private JLabel lblNbCardRemaining;
-    
+    private JLabel lblInfo;
+
     public JLabel getLblNbCardRemaining() {
         return this.lblNbCardRemaining;
     }
-
+    
     public void setLblNbCardRemaining(JLabel lblNbCardRemaining) {
         this.lblNbCardRemaining = lblNbCardRemaining;
     }
-
+    
+    public JLabel getLblInfo() {
+        return this.lblInfo;
+    }
+    
+    public void setLblInfo(JLabel lblInfo) {
+        this.lblInfo = lblInfo;
+    }
+    
 }
